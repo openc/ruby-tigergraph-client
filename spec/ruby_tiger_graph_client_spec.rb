776fd380f45f3fa59105c94c8907177a67b025b0
benjessop12 <ben.jessop@opencorporates.com>
2020-07-28 15:38:40 +0100

    Move spec

--- spec/ruby_tiger_graph_client_spec.rb
+++ spec/ruby_tiger_graph_client_spec.rb
@@ -1,5 +1,5 @@
 require "spec_helper"
-require "./lib/ruby_tiger_graph_client"
+require "./lib/ruby_tiger_graph_client.rb"
 #mock this all
 
 RSpec.describe TigerGraphClient, use_graph_database: true do # rubocop:disable Style/MultilineIfModifier
